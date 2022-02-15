76baffdb27b0af7ffb151c20afc30318b19d2ae7
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-02-15 15:47:29 -0600

    Initial Commit

---
+++ Shared/QueueGifsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueueGifsApp.swift
+//  Shared
+//
+//  Created by Borna Libertines on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct QueueGifsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
