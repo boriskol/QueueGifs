183723d1182917ecc8fc8e06e5f83283d148f0b3
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-02-16 05:56:37 -0600

    intent

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,97 +6,93 @@
 //
 /*
  ///A queue is a list where you can only insert new items at the back and remove items from the front.
-    This ensures that the first item you enqueue is also the first item you dequeue. First come, first serve!
-
+ This ensures that the first item you enqueue is also the first item you dequeue. First come, first serve!
+ 
  ///Why would you need this? Well, in many algorithms you want to add objects to a temporary list and pull them off this list later. Often the order in which you add and remove these objects matters.
-
+ 
  //A queue gives you a FIFO or first-in, first-out order. The element you inserted first is the first one to come out. It is only fair! (A similar data structure, the stack, is LIFO or last-in first-out.)
  */
 import SwiftUI
 
 struct ContentView: View {
    
-    
-    @ObservedObject private var gifs = MainViewModel()
-    
-    var body: some View {
-        NavigationView{
-                GeometryReader { geometry in
-                   
-                    VStack(alignment: .leading, spacing: 0, content: {
-                        // MARK: Stack
-                        
-                        //if let front = self.gifs.front1{
-                            Section(header: VStack(alignment: .leading, spacing: 8){
-                                HStack(){
-                                    
-                                    Spacer()
-                                    Button(action: {
-                                        self.gifs.dequeue()
-                                        }) {
-                                            HStack {
-                                                Image(systemName: "minus")
-                                                Text("dequeue")
-                                            }.padding(10.0)
-                                            .overlay(RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 2.0))
-                                        }
-                                }
-                               
-                            }, content: {
-                                VStack{
-                                    if self.gifs.giffront != nil{
-                                        GifCell(gif: self.gifs.giffront!, geometry: geometry)
-                                    }
-                                }.listStyle(.plain)
-                            })
-                        //}
-                        if !self.gifs.isEmpty{
-                        Section(header: VStack(alignment: .leading, spacing: 8){
-                            Text("Gifs Stack").font(.body).foregroundColor(.purple).fontWeight(.bold).padding(.leading)
-                        }, content: {
-                            Text("count: \(self.gifs.count)")
-                            
-                            List{
-                                ForEach(self.gifs.gifsStack, id: \.id) { gif in
-                                    GifCell(gif: gif, geometry: geometry)
-                                }
-                            }.listStyle(.plain)
-                        })
-                        }
-                        // MARK: Gifs
-                        Section(header: VStack(alignment: .leading, spacing: 8){
-                            Text("Gifs Traiding").font(.body).foregroundColor(.purple).fontWeight(.bold).padding(.leading).onAppear{
-                            }
-                        }, content: {
-                            List{
-                                ForEach(self.gifs.gifs, id: \.id) { gif in
-                                    GifCell(gif: gif, geometry: geometry)
-                                        .onTapGesture {
-                                            self.gifs.enqueue(gif)
-                                        }
-                                }
-                            }.listStyle(.plain)
-                        })
-                    })
-                    .task{
-                        await gifs.loadGift()
-                        }
-                    .refreshable {
-                        await gifs.loadGift()
-                    }
-                }//gio
+   
+   @ObservedObject private var gifs = MainViewModel()
+   
+   var body: some View {
+      NavigationView{
+         GeometryReader { geometry in
             
-            
-            .hideNavigationBar()
-        }//nav
-        .edgesIgnoringSafeArea(.all)
-        .navigationViewStyle(StackNavigationViewStyle())
-    }
+            VStack(alignment: .leading, spacing: 0, content: {
+               // MARK: Stack
+               Section(header: VStack(alignment: .leading, spacing: 8){
+                  HStack(){
+                     Spacer()
+                     Button(action: {
+                        self.gifs.dequeue()
+                     }) {
+                        HStack {
+                           Image(systemName: "minus")
+                           Text("dequeue")
+                        }.padding(10.0)
+                           .overlay(RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 2.0))
+                     }
+                  }
+                  
+               }, content: {
+                  VStack{
+                     if self.gifs.giffront != nil{
+                        GifCell(gif: self.gifs.giffront!, geometry: geometry)
+                     }
+                  }.listStyle(.plain)
+               })
+               
+               Section(header: VStack(alignment: .leading, spacing: 8){
+                  Text("Gifs Stack").font(.body).foregroundColor(.purple).fontWeight(.bold).padding(.leading)
+               }, content: {
+                  Text("count: \(self.gifs.count)")
+                  
+                  List{
+                     ForEach(self.gifs.gifsStack, id: \.id) { gif in
+                        GifCell(gif: gif, geometry: geometry)
+                     }
+                  }.listStyle(.plain)
+               })
+               
+               // MARK: Gifs
+               Section(header: VStack(alignment: .leading, spacing: 8){
+                  Text("Gifs Trading").font(.body).foregroundColor(.purple).fontWeight(.bold).padding(.leading).onAppear{
+                  }
+               }, content: {
+                  List{
+                     ForEach(self.gifs.gifs, id: \.id) { gif in
+                        GifCell(gif: gif, geometry: geometry)
+                           .onTapGesture {
+                              self.gifs.enqueue(gif)
+                           }
+                     }
+                  }.listStyle(.plain)
+               })
+            })
+               .task{
+                  await gifs.loadGift()
+               }
+               .refreshable {
+                  await gifs.loadGift()
+               }
+         }//gio
+         
+         
+         .hideNavigationBar()
+      }//nav
+      .edgesIgnoringSafeArea(.all)
+      .navigationViewStyle(StackNavigationViewStyle())
+   }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+      ContentView()
+   }
 }
--- Shared/ViewModel.swift
+++ Shared/ViewModel.swift
@@ -10,105 +10,104 @@ import Combine
 import SwiftUI
 
 public struct Queue<T> {
-  fileprivate var array = [T?]()
-  fileprivate var head = 0
-  
-  public var isEmpty: Bool {
-    return count == 0
-  }
-
-  public var count: Int {
-    return array.count - head
-  }
-  
-  public mutating func enqueue(_ element: T) {
-    array.append(element)
-  }
-  
-  public mutating func dequeue() -> T? {
-    guard head < array.count, let element = array[head] else { return nil }
-
-    array[head] = nil
-    head += 1
-
-    //let percentage = Double(head)/Double(array.count)
-    //if array.count > 50 && percentage > 0.25 {
-    if head > 2{
-      array.removeFirst(head)
-      head = 0
-    }
-    
-    return element
-  }
-  
-  public var front: T? {
-    if isEmpty {
-      return nil
-    } else {
-      return array[head]
-    }
-  }
+   fileprivate var array = [T?]()
+   fileprivate var head = 0
+   
+   public var isEmpty: Bool {
+      return count == 0
+   }
+   
+   public var count: Int {
+      return array.count - head
+   }
+   
+   public mutating func enqueue(_ element: T) {
+      array.append(element)
+   }
+   
+   public mutating func dequeue() -> T? {
+      guard head < array.count, let element = array[head] else { return nil }
+      
+      array[head] = nil
+      head += 1
+      
+      //let percentage = Double(head)/Double(array.count)
+      //if array.count > 50 && percentage > 0.25 {
+      if head > 2{
+         array.removeFirst(head)
+         head = 0
+      }
+      
+      return element
+   }
+   
+   public var front: T? {
+      if isEmpty {
+         return nil
+      } else {
+         return array[head]
+      }
+   }
 }
 
 // MARK: MainViewModel
 
 class MainViewModel: ObservableObject {
-    
-    @Published private(set) var gifsStack = [GifCollectionViewCellViewModel]()
-    @Published private(set) var giffront: GifCollectionViewCellViewModel?
-    private var head = 0
-    
-    @Published private(set) var gifs = [GifCollectionViewCellViewModel]()
-    
-    // MARK:  Initializer Dependency injestion
-    var appiCall: ApiLoader?
-    
-    init(appiCall: ApiLoader = ApiLoader()){
-        self.appiCall = appiCall
-    }
-    
-    public var isEmpty: Bool {
-        return count == 0
-    }
-    public var count: Int {
-        return gifsStack.count - head
-    }
-    public func enqueue(_ element: GifCollectionViewCellViewModel){
-        gifsStack.append(element)
-    }
-    public func dequeue() -> GifCollectionViewCellViewModel?{
-        
-        guard !gifsStack.isEmpty else { return nil}
-        guard head <= gifsStack.count else { return nil }
-        let element1 = gifsStack[head]
-        
-        gifsStack[head] = GifCollectionViewCellViewModel(id: nil, title: nil, rating: nil, Image: nil, url: nil)
-        head += 1
-        
-        //let percentage = Double(head)/Double(gifsStack.count)
-        //if gifsStack.count > 50 && percentage > 0.25 {
-        if head > 2 {
-            gifsStack.removeFirst(head)
-            head = 0
-        }
-       
-        return element1
-    }
    
-    
-    @MainActor func loadGift() async {
-        
-        Task(priority: .userInitiated, operation: {
-            let fp: APIListResponse? = try? await appiCall?.fetchAPI(urlParams: [Constants.rating: Constants.rating, Constants.limit: Constants.limitNum], gifacces: Constants.trending)
-            let d = fp?.data.map({ return GifCollectionViewCellViewModel(id: $0.id, title: $0.title, rating: $0.rating, Image: $0.images?.fixed_height?.url, url: $0.url)
-            })
-            self.gifs = d!
-        })
-    }
+   @Published private(set) var gifsStack = [GifCollectionViewCellViewModel]()
+   @Published private(set) var giffront: GifCollectionViewCellViewModel?
+   private var head = 0
    
-
-    deinit{
-        
-        debugPrint("MainViewModel deinit")
-    }
+   @Published private(set) var gifs = [GifCollectionViewCellViewModel]()
+   
+   // MARK:  Initializer Dependency injestion
+   var appiCall: ApiLoader?
+   
+   init(appiCall: ApiLoader = ApiLoader()){
+      self.appiCall = appiCall
+   }
+   
+   public var isEmpty: Bool {
+      return count == 0
+   }
+   public var count: Int {
+      return gifsStack.count - head
+   }
+   public func enqueue(_ element: GifCollectionViewCellViewModel){
+      gifsStack.append(element)
+   }
+   public func dequeue() -> GifCollectionViewCellViewModel?{
+      
+      guard !gifsStack.isEmpty else { return nil}
+      guard head <= gifsStack.count else { return nil }
+      let element1 = gifsStack[head]
+      
+      gifsStack[head] = GifCollectionViewCellViewModel(id: nil, title: nil, rating: nil, Image: nil, url: nil)
+      head += 1
+      
+      //let percentage = Double(head)/Double(gifsStack.count)
+      //if gifsStack.count > 50 && percentage > 0.25 {
+      if head > 2 {
+         gifsStack.removeFirst(head)
+         head = 0
+      }
+      
+      return element1
+   }
+   
+   
+   @MainActor func loadGift() async {
+      
+      Task(priority: .userInitiated, operation: {
+         let fp: APIListResponse? = try? await appiCall?.fetchAPI(urlParams: [Constants.rating: Constants.rating, Constants.limit: Constants.limitNum], gifacces: Constants.trending)
+         let d = fp?.data.map({ return GifCollectionViewCellViewModel(id: $0.id, title: $0.title, rating: $0.rating, Image: $0.images?.fixed_height?.url, url: $0.url)
+         })
+         self.gifs = d!
+      })
+   }
+   
+   
+   deinit{
+      debugPrint("MainViewModel deinit")
+   }
 }
