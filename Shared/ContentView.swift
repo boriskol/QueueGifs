31bed84c5db489c809e32e51f828e4b38d6cbcaf
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-02-15 18:29:36 -0600

    queue

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -4,15 +4,102 @@
 //
 //  Created by Borna Libertines on 15/02/22.
 //
+/*
+ ///A queue is a list where you can only insert new items at the back and remove items from the front.
+    This ensures that the first item you enqueue is also the first item you dequeue. First come, first serve!
 
+ ///Why would you need this? Well, in many algorithms you want to add objects to a temporary list and pull them off this list later. Often the order in which you add and remove these objects matters.
+
+ //A queue gives you a FIFO or first-in, first-out order. The element you inserted first is the first one to come out. It is only fair! (A similar data structure, the stack, is LIFO or last-in first-out.)
+ */
 import SwiftUI
 
 struct ContentView: View {
+   
+    
+    @ObservedObject private var gifs = MainViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+                GeometryReader { geometry in
+                    /*if let tit = self.gifs.gif?.title{
+                        NavigationLink(destination: VStack{Text("Detail View \(tit)")}, isActive: self.$gifs.gifDetail) {
+                            Text("").frame(width: 0, height: 0)
+                        }
+                    }*/
+                    VStack(alignment: .leading, spacing: 0, content: {
+                        // MARK: Stack
+                        
+                        if let front = self.gifs.front{
+                            Section(header: VStack(alignment: .leading, spacing: 8){
+                                HStack(){
+                                    Text("Gif Stack Top").font(.body).foregroundColor(.purple).fontWeight(.bold).padding(.leading)
+                                    Spacer()
+                                    Button(action: {
+                                        self.gifs.dequeue()
+                                        }) {
+                                            HStack {
+                                                Image(systemName: "minus")
+                                                Text("dequeue")
+                                            }.padding(10.0)
+                                            .overlay(RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 2.0))
+                                        }
+                                }
+                               
+                            }, content: {
+                                VStack{
+                                    GifCell(gif: front, geometry: geometry)
+                                }.listStyle(.plain)
+                            })
+                        }
+                        if !self.gifs.isEmpty{
+                        Section(header: VStack(alignment: .leading, spacing: 8){
+                            Text("Gifs Stack").font(.body).foregroundColor(.purple).fontWeight(.bold).padding(.leading)
+                        }, content: {
+                            Text("count: \(self.gifs.count)")
+                            
+                            List{
+                                ForEach(self.gifs.gifsStack, id: \.id) { gif in
+                                    GifCell(gif: gif, geometry: geometry)
+                                }
+                            }.listStyle(.plain)
+                        })
+                        }
+                        // MARK: Gifs
+                        Section(header: VStack(alignment: .leading, spacing: 8){
+                            Text("Gifs Traiding").font(.body).foregroundColor(.purple).fontWeight(.bold).padding(.leading).onAppear{
+                            }
+                        }, content: {
+                            List{
+                                ForEach(self.gifs.gifs, id: \.id) { gif in
+                                    GifCell(gif: gif, geometry: geometry)
+                                        .onTapGesture {
+                                            //debugPrint(gif)
+                                            self.gifs.enqueue(gif)
+                                        }
+                                }
+                            }.listStyle(.plain)
+                        })
+                    })
+                    .task{
+                        await gifs.loadGift()
+                        //await gifs.search(search: "love")
+                        //let searchId = self.gifs.gifs[0].id
+                        //await gifs.searchGifId(gifID: searchId!)
+                    }
+                    .refreshable {
+                        await gifs.loadGift()
+                    }
+                }//gio
+            
+            
+            .hideNavigationBar()
+        }//nav
+        .edgesIgnoringSafeArea(.all)
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Shared/ViewModel.swift
@@ -0,0 +1,117 @@
+//
+//  ViewModel.swift
+//  StackGif
+//
+//  Created by Borna Libertines on 14/02/22.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+public struct Queue<T> {
+  fileprivate var array = [T?]()
+  fileprivate var head = 0
+  
+  public var isEmpty: Bool {
+    return count == 0
+  }
+
+  public var count: Int {
+    return array.count - head
+  }
+  
+  public mutating func enqueue(_ element: T) {
+    array.append(element)
+  }
+  
+  public mutating func dequeue() -> T? {
+    guard head < array.count, let element = array[head] else { return nil }
+
+    array[head] = nil
+    head += 1
+
+    //let percentage = Double(head)/Double(array.count)
+    //if array.count > 50 && percentage > 0.25 {
+    if head > 2{
+      array.removeFirst(head)
+      head = 0
+    }
+    
+    return element
+  }
+  
+  public var front: T? {
+    if isEmpty {
+      return nil
+    } else {
+      return array[head]
+    }
+  }
+}
+
+// MARK: MainViewModel
+
+class MainViewModel: ObservableObject {
+    
+    @Published private(set) var gifsStack = [GifCollectionViewCellViewModel]()
+    fileprivate var head = 0
+    
+    @Published private(set) var gifs = [GifCollectionViewCellViewModel]()
+    
+    // MARK:  Initializer Dependency injestion
+    var appiCall: ApiLoader?
+    
+    init(appiCall: ApiLoader = ApiLoader()){
+        self.appiCall = appiCall
+    }
+    
+    public var isEmpty: Bool {
+        return count == 0
+    }
+    public var count: Int {
+        return gifsStack.count - head
+    }
+    public func enqueue(_ element: GifCollectionViewCellViewModel){
+        gifsStack.append(element)
+    }
+    public func dequeue() -> GifCollectionViewCellViewModel?{
+        let element = gifsStack[head]
+        guard head < gifsStack.count else { return nil }
+        
+        gifsStack[head] = GifCollectionViewCellViewModel(id: nil, title: nil, rating: nil, Image: nil, url: nil)
+            head += 1
+        //let percentage = Double(head)/Double(gifsStack.count)
+        //if gifsStack.count > 50 && percentage > 0.25 {
+        if head > 2 {
+            gifsStack.removeFirst(head)
+            head = 0
+        }
+        //debugPrint(element)
+        return element
+    }
+    public var front: GifCollectionViewCellViewModel? {
+        if isEmpty {
+              return nil
+        } else {
+            return gifsStack[head]
+        }
+       
+    }
+    
+    @MainActor func loadGift() async {
+        
+        Task(priority: .userInitiated, operation: {
+            let fp: APIListResponse? = try? await appiCall?.fetchAPI(urlParams: [Constants.rating: Constants.rating, Constants.limit: Constants.limitNum], gifacces: Constants.trending)
+            let d = fp?.data.map({ return GifCollectionViewCellViewModel(id: $0.id, title: $0.title, rating: $0.rating, Image: $0.images?.fixed_height?.url, url: $0.url)
+            })
+            self.gifs = d!
+        })
+    }
+   
+
+    deinit{
+        
+        debugPrint("MainViewModel deinit")
+    }
+}
